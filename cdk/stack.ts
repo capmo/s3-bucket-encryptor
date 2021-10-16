75f4cf3da8f74528e3da2bc9c0948494110ceeae
Klemens Mang <klemens.mang@capmo.de>
2021-10-17 01:39:42 +0200

    Added circleCI pipeline

--- cdk/stack.ts
+++ cdk/stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from "@aws-cdk/core";
+import { name } from '../package.json';
 
 export class CloudStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -7,12 +8,16 @@ export class CloudStack extends cdk.Stack {
 }
 
 export const {
+  account = process.env.CDK_DEFAULT_ACCOUNT,
   environment = "dev",
   region = "eu-central-1",
-  account = process.env.CDK_DEFAULT_ACCOUNT,
+  stage = 'dev',
 } = process.env.CDK_CONTEXT_JSON
   ? JSON.parse(process.env.CDK_CONTEXT_JSON)
   : {};
 
 const app = new cdk.App();
-export const stack = new CloudStack(app, "s3-bucket-encryptor", { env: { region, account } });
+export const stack = new CloudStack(app, "s3-bucket-encryptor", { 
+  env: { region, account }, 
+  stackName: name + (stage === 'prod' ? '' : `-${stage}`), 
+});

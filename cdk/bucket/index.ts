14b392bd2238176a78ec5be2bf4d04928e9ded09
Klemens Mang <klemens.mang@capmo.de>
2021-10-23 12:52:08 +0200

    Fix access denied errors

--- cdk/bucket/index.ts
+++ cdk/bucket/index.ts
@@ -1,19 +1,21 @@
 import { RemovalPolicy } from '@aws-cdk/aws-cloudwatch/node_modules/@aws-cdk/core';
-import { SecureBucket } from '@capmo/cdk-lib/lib/s3';
+import { BlockPublicAccess, Bucket } from '@aws-cdk/aws-s3';
 import { stack } from '../stack';
 
-export const inventoryReportBucket = new SecureBucket(
+export const inventoryReportBucket = new Bucket(
   stack,
   'InventoryReportBucket',
   {
+    blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
     removalPolicy: RemovalPolicy.RETAIN,
   }
 );
 
-export const batchReportBucket = new SecureBucket(
+export const batchReportBucket = new Bucket(
   stack,
   'S3BatchReportsDestinationBucket',
   {
+    blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
     removalPolicy: RemovalPolicy.RETAIN,
   }
 );
